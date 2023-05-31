6f53ddb33e201a713e231fc9cc59c366a6f653de
Haroon Khaliqi <haroonkhaliqi12@Haroons-MBP.attlocal.net>
2023-05-31 13:34:20 -0700

    create associtations

--- app/models/room.rb
+++ app/models/room.rb
@@ -1,2 +1,5 @@
 class Room < ApplicationRecord
+  has_many :reservation
+  has_many :room_images
+  belongs_to :user
 end
--- app/models/room_image.rb
+++ app/models/room_image.rb
@@ -1,2 +1,3 @@
 class RoomImage < ApplicationRecord
+  belongs_to :room
 end
