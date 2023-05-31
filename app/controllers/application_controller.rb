fd001a6e9006171ee993c65a4d08dc839af86865
Haroon Khaliqi <haroonkhaliqi12@Haroons-MBP.attlocal.net>
2023-05-31 14:01:30 -0700

    created applcation controller and reservations controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,33 @@
 class ApplicationController < ActionController::Base
+  protect_from_forgery with: :exception, unless: -> { request.format.json? }
+  
+  def current_user
+    auth_headers = request.headers["Authorization"]
+    if auth_headers.present? && auth_headers[/(?<=\A(Bearer ))\S+\z/]
+      token = auth_headers[/(?<=\A(Bearer ))\S+\z/]
+      begin
+        decoded_token = JWT.decode(
+          token,
+          Rails.application.credentials.fetch(:secret_key_base),
+          true,
+          { algorithm: "HS256" }
+        )
+        User.find_by(id: decoded_token[0]["user_id"])
+      rescue JWT::ExpiredSignature
+        nil
+      end
+    end
+  end
+
+  def authenticate_user
+    unless current_user
+      render json: {}, status: :unauthorized
+    end
+  end
+
+  def authenticate_admin
+    unless current_user
+      render json: {}, status: :unauthorized
+    end
+  end
 end
--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -1,2 +1,27 @@
 class ReservationsController < ApplicationController
+  before_action :authenticate_user
+
+  def index
+    @reservations = Reservation.all
+    
+    render :index
+  end
+
+  def show
+    @reservation = current_user.reservations.find_by(id: params["id"])
+
+    render :show
+  end
+
+  def create
+    @reservation = Reservation.create(
+      user_id: current_user.id,
+      room_id: current_user.room.id,
+      start_date: params["start_date"],
+      end_date: params["end_date"],
+      price: current_user.room.price
+    )
+
+    render :show
+  end
 end
