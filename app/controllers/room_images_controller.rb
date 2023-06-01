9cb477908067f9c0e56f2521dfc04b0b79c6edf3
Haroon Khaliqi <haroonkhaliqi12@Haroons-MBP.attlocal.net>
2023-06-01 10:49:06 -0700

    created roomimage and the routes for all cotnroller

--- app/controllers/room_images_controller.rb
+++ app/controllers/room_images_controller.rb
@@ -1,2 +1,3 @@
 class RoomImagesController < ApplicationController
-
+  
+end
--- app/models/room_image.rb
+++ app/models/room_image.rb
@@ -1,3 +1,4 @@
 class RoomImage < ApplicationRecord
   belongs_to :room
+  validates_format_of :url, :with => %r{\.(png|jpg|jpeg|gif)$}i, :message => "image must be png, jpg, or jpeg", multiline: true
 end
