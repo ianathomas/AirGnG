1f472defff64ef3e724f6ab2897dc22cb5794636
Haroon Khaliqi <haroonkhaliqi12@Haroons-MBP.attlocal.net>
2023-06-01 12:14:37 -0700

    removed roomimages controller

--- app/controllers/room_images_controller.rb
+++
@@ -1,1 +0,0 @@
-class RoomImagesController < ApplicationController
--- db/seeds.rb
+++ db/seeds.rb
@@ -7,8 +7,8 @@
 #   Character.create(name: "Luke", movie: movies.first)
 Room.create(user_id: 1, planet: "Mars", price: 50000, description: "Lovely orange hues and sweeping hills of dirt", total_occupancy: 12, total_bedrooms: 8, total_bathrooms: 6, home_type: "Cave System", section: "Lower Hemisphere")
 
-Room_Image.create(room_id: 1, url: "https://universemagazine.com/wp-content/uploads/2022/05/fstinwtxeaqngou-768x568.jpg")
+RoomImage.create(room_id: 1, url: "https://universemagazine.com/wp-content/uploads/2022/05/fstinwtxeaqngou-768x568.jpg")
 
-Reservation.create(user_id: 1, room_id: 1, start_date: "July 17th, 2030", end_date: "August 12th, 2030", price: 50000)
+Reservation.create(user_id: 1, room_id: 1, start_date: "July 18th, 2030", end_date: "August 12th, 2030", price: 50000)
 
 Review.create(reservation_id: 1, rating: 3, comment: "Absolutely no water available but scenery was great")
