44edf5c108217089da7245ec0733c9ff9c1413af
RaineHanley <rainehanley@gmail.com>
2023-06-01 09:40:41 -0700

    Created users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,16 @@
 class UsersController < ApplicationController
+  def create
+    user = User.new(
+      name: params[:name],
+      email: params[:email],
+      password: params[:password],
+      password_digest: params[:password_digest],
+      image: params[:image],
+    )
+    if user.save
+      render json: { message: "User created successfully" }, status: :created
+    else
+      render json: { errors: user.errors.full_messages }, status: :bad_request
+    end
+  end
 end
