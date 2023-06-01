b56d06b014e90b15d9b71499b08cfb654289c33a
Haroon Khaliqi <haroonkhaliqi12@Haroons-MBP.attlocal.net>
2023-06-01 11:48:07 -0700

    fixed authentication

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,33 +1,10 @@
 class ApplicationController < ActionController::Base
-  protect_from_forgery with: :exception, unless: -> { request.format.json? }
-  
   def current_user
-    auth_headers = request.headers["Authorization"]
-    if auth_headers.present? && auth_headers[/(?<=\A(Bearer ))\S+\z/]
-      token = auth_headers[/(?<=\A(Bearer ))\S+\z/]
-      begin
-        decoded_token = JWT.decode(
-          token,
-          Rails.application.credentials.fetch(:secret_key_base),
-          true,
-          { algorithm: "HS256" }
-        )
-        User.find_by(id: decoded_token[0]["user_id"])
-      rescue JWT::ExpiredSignature
-        nil
-      end
-    end
+    @current_user ||= User.find_by(id: session[:user_id]) if session[:user_id]
   end
-
+  helper_method :current_user
+ 
   def authenticate_user
-    unless current_user
-      render json: {}, status: :unauthorized
-    end
-  end
-
-  def authenticate_admin
-    unless current_user
-      render json: {}, status: :unauthorized
-    end
+    redirect_to '/login', status: :see_other unless current_user
   end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,16 +1,21 @@
 class UsersController < ApplicationController
+  def new
+    @user = User.new
+    render template: "users/new"
+  end
+
   def create
-    user = User.new(
-      name: params[:name],
-      email: params[:email],
-      password: params[:password],
-      password_digest: params[:password_digest],
-      image: params[:image],
+    @user = User.new(
+      name: params[:user][:name],
+      email: params[:user][:email],
+      password: params[:user][:password],
+      password_confirmation: params[:user][:password_confirmation],
     )
-    if user.save
-      render json: { message: "User created successfully" }, status: :created
+    if @user.save
+      session[:user_id] = @user.id
+      redirect_to "/"
     else
-      render json: { errors: user.errors.full_messages }, status: :bad_request
+      render :new, status: :unprocessable_entity
     end
   end
 end
