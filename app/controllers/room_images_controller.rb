55d28255b7bba2ce48ac3fb5b2f467a045fe5807
RaineHanley <rainehanley@gmail.com>
2023-06-01 10:23:16 -0700

    Room Controller Created

--- app/controllers/room_images_controller.rb
+++ app/controllers/room_images_controller.rb
@@ -1,2 +1,60 @@
 class RoomImagesController < ApplicationController
+  before_action :authenticate_admin, except: [:index, :show]
+
+  def index
+    @rooms = Room.all
+    render :index
+  end
+
+  def show
+    @room = Room.find_by(id: params["id"])
+    render :show
+  end
+
+  def create
+    @room = Room.create({
+      user_id: params["name"],
+      planet: params["planet"],
+      price: params["price"],
+      total_occupancy: params["total_occupancy"],
+      total_bedrooms: params["total_bedrooms"],
+      total_bathrooms: params["total_bathrooms"],
+      home_type: params[" home_type"],
+      section: params["section"],
+    })
+
+    if @room.valid?
+      Image.create(room_id: @room.id, url: params["image_url"])
+      render :show
+    else
+      render json: { errors: @product.errors.full_messages }, status: :unprocessable_entity
+    end
+  end
+
+  def update
+    @room = Room.find_by(id: params["id"])
+    @room.update(
+      user_id: params["name"],
+      planet: params["planet"],
+      price: params["price"],
+      total_occupancy: params["total_occupancy"],
+      total_bedrooms: params["total_bedrooms"],
+      total_bathrooms: params["total_bathrooms"],
+      home_type: params[" home_type"],
+      section: params["section"],
+    )
+
+    if @room.valid?
+      render :show
+    else
+      render json: { errors: @product.errors.full_messages }, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @room = Room.find_by(id: params["id"])
+    @room.destroy
+
+    render json: { message: "Room Destruction Completed" }
+  end
 end
