b500d9a33a8fddb9009f41c7ece725b9d1d0b6cb
RaineHanley <rainehanley@gmail.com>
2023-06-02 10:38:04 -0700

    added 3 seeds that WORK

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,7 +5,7 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
-user = User.create!(name: "Nick", email: "email@email.com", password: "password")
+user = User.create!(name: "Nick", email: "nick@test.com", password: "password")
 
 room = Room.create!(user_id: user.id, planet: "Mars", price: 50000, description: "Lovely orange hues and sweeping hills of dirt", total_occupancy: 12, total_bedrooms: 8, total_bathrooms: 6, home_type: "Cave System", section: "Lower Hemisphere")
 
@@ -15,22 +15,21 @@ reservation = Reservation.create!(user_id: user.id, room_id: room.id, start_date
 
 Review.create!(reservation_id: reservation.id, rating: 3, comment: "Absolutely no water available but scenery was great")
 
-user = User.create!(name: "Stu", email: "stu@email.com", password: "password")
+user = User.create!(name: "Stu", email: "stu@test.com", password: "password")
 
-room = Room.create!(user_id: user.id, planet: "Vedriwei", price: 2000000, description: "Freezing cold temperatures, perfect for those who have ice cold hearts" total_occupancy: 1, total_bedrooms: 1, total_bathrooms: 1, home_type: "Ice Pit", section: "Lower Hemisphere")
+room = Room.create!(user_id: user.id, planet: "Vedriwei", price: 2000000, description: "Freezing cold temperatures, perfect for those who have ice cold hearts", total_occupancy: 1, total_bedrooms: 1, total_bathrooms: 1, home_type: "Ice Pit", section: "Lower Hemisphere")
 
-RoomImage.create!(room_id: room.id, url:"https://t4.ftcdn.net/jpg/05/63/71/89360_F_563718941_3IQJ9plkgdFWs0kIeZTLgWMIWtqyzsLL.jpg")
+RoomImage.create!(room_id: room.id, url: "https://t4.ftcdn.net/jpg/05/63/71/89360_F_563718941_3IQJ9plkgdFWs0kIeZTLgWMIWtqyzsLL.jpg")
 
 reservation = Reservation.create!(user_id: user.id, room_id: room.id, start_date: "May 14, 2042", end_date: "May 14, 2042", price: 2000000)
 
 Review.create!(reservation_id: reservation.id, rating: 5, comment: "My year on planet Vedriwei was the best decision of my life. As I hate all other beings this planet was cold, isolating, and perfect. ")
 
+user = User.create!(name: "Blarb", email: "Blarb@test.com", password: "password")
 
-user = User.create!(name: "Blarb", email: "Blarb@email.com", password: "password")
+room = Room.create!(user_id: user.id, planet: "Cropirus", price: 40, description: "Extremely Windy. Rent with caution", total_occupancy: 10, total_bedrooms: 6, total_bathrooms: 1, home_type: "Ice Pit", section: "Lower Hemisphere")
 
-room = Room.create!(user_id: user.id, planet: "Cropirus", price: 40, description: "Extremely Windy. Rent with caution" total_occupancy: 10, total_bedrooms: 6, total_bathrooms: , home_type: "Ice Pit", section: "Lower Hemisphere")
-
-RoomImage.create!(room_id: room.id, url:"https://www.weatherwizkids.com/wp-content/uploads/2015/04/sandstorm2.jpg")
+RoomImage.create!(room_id: room.id, url: "https://www.weatherwizkids.com/wp-content/uploads/2015/04/sandstorm2.jpg")
 
 reservation = Reservation.create!(user_id: user.id, room_id: room.id, start_date: "August 14, 3040", end_date: "August 20, 3040", price: 40)
 
